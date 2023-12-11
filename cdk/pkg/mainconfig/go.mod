e43a6bfebc7f936a4e56be6160b7244ee2943c80
sylvain <sylvain-combe-sonarsource@users.noreply.github.com>
2023-12-11 11:30:22 +0100

    missing go dependencies for deveops scripts

---
+++ cdk/pkg/mainconfig/go.mod
@@ -0,0 +1,3 @@
+module CDK/pkg/mainconfig
+
+go 1.21.1
---
+++ cdk/pkg/mainconfig/mainconfig.go
@@ -0,0 +1,9 @@
+package mainconfig
+
+type ConfAuth struct {
+	Region     string
+	Account    string
+	SSOProfile string
+	Index      string
+	AWSsecret  string
+}
